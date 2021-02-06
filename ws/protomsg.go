901d6ddb51a6ff2083bde556da812062a568059f
Fabio Tranchitella <fabio.tranchitella@northern.tech>
2021-02-06 07:11:27 +0100

    ws: add constants for the port forwarding feature
    Changelog: none
    
    Signed-off-by: Fabio Tranchitella <fabio.tranchitella@northern.tech>

--- ws/protomsg.go
+++ ws/protomsg.go
@@ -1,4 +1,4 @@
-// Copyright 2020 Northern.tech AS
+// Copyright 2021 Northern.tech AS
 //
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
@@ -26,6 +26,8 @@ const (
 	ProtoTypeShell
 	// ProtoTypeFileTransfer is used for file transfer from/to the device.
 	ProtoTypeFileTransfer
+	// ProtoTypePortForward is used for port-forwarding connections to the device.
+	ProtoTypePortForward
 )
 
 // ProtoHdr provides the info about what the ProtoMsg contains and

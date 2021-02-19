8df96dfc58a7c65bb946fb0046f3c80263c6b73f
Alf-Rune Siqveland <alf.rune@northern.tech>
2021-02-19 10:13:43 +0100

    filetransfer: Make schema parameters pointers
    We need pointers for optional parameters, but let's make it consistent
    accross all fields.
    
    changelog: none
    
    Signed-off-by: Alf-Rune Siqveland <alf.rune@northern.tech>

--- ws/filetransfer/model.go
+++ ws/filetransfer/model.go
@@ -14,6 +14,8 @@
 
 package filetransfer
 
+import "time"
+
 const (
 	// MessageTypeGet requests a file from the device.
 	MessageTypeGet = "get_file"
@@ -41,37 +43,39 @@ const (
 // The Error struct is passed in the Body of MsgProto in case the message type is ErrorMessage
 type Error struct {
 	// The error description, as in "Permission denied while opening a file"
-	Error string `msgpack:"err" json:"error"`
+	Error *string `msgpack:"err" json:"error"`
 	// Type of message that raised the error
-	MessageType string `msgpack:"msgtype,omitempty" json:"message_type,omitempty"`
+	MessageType *string `msgpack:"msgtype,omitempty" json:"message_type,omitempty"`
 	// Message id is passed in the MsgProto Properties, and in case it is available and
 	// error occurs it is passed for reference in the Body of the error message
-	MessageID string `msgpack:"msgid,omitempty" json:"message_id,omitempty"`
+	MessageID *string `msgpack:"msgid,omitempty" json:"message_id,omitempty"`
 }
 
 // Get file requests the flow of MessageTypeFileChunk messages to be started from the remote end
 type GetFile struct {
 	// The file path to the file we are requesting
-	Path string `msgpack:"path,omitempty" json:"path,omitempty"`
+	Path *string `msgpack:"path,omitempty" json:"path,omitempty"`
 }
 
 // Stat file requests the file stat structure from the remote end
 type StatFile struct {
 	// The file path to the file we are requesting
-	Path string `msgpack:"path,omitempty" json:"path,omitempty"`
+	Path *string `msgpack:"path" json:"path,omitempty"`
 }
 
 // FileInfo is the object returned from a StatFile request and is also used
 // for "put_file" requests for specifying the target file.
 type FileInfo struct {
 	// The file path to the file we are sending status for
-	Path string `msgpack:"path,omitempty" json:"path,omitempty"`
+	Path *string `msgpack:"path" json:"path"`
 	// The file size
-	Size int64 `msgpack:"size,omitempty" json:"size,omitempty"`
+	Size *int64 `msgpack:"size,omitempty" json:"size,omitempty"`
 	// The file owner
-	UID uint32 `msgpack:"uid,omitempty" json:"uid,omitempty"`
+	UID *uint32 `msgpack:"uid,omitempty" json:"uid,omitempty"`
 	// The file group
-	GID uint32 `msgpack:"gid,omitempty" json:"gid,omitempty"`
+	GID *uint32 `msgpack:"gid,omitempty" json:"gid,omitempty"`
 	// Mode contains the file mode and permission bits.
-	Mode uint32 `msgpack:"mode,omitempty" json:"mode,omitempty"`
+	Mode *uint32 `msgpack:"mode,omitempty" json:"mode,omitempty"`
+	// ModTime is the last modification time for the file.
+	ModTime *time.Time `msgpack:"modtime,omitempty" json:"modification_time,omitempty"`
 }
